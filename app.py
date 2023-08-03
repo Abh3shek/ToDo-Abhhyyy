8a260864abb8998efe69c9d9fd212d1db3ce2882
ABHee <abhishekganapure22@gmail.com>
2023-08-03 22:58:39 +0530

    random commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 db = SQLAlchemy(app)
 
@@ -48,4 +49,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         
-    app.run(debug=True, host='127.7.3.9', port='8080')
+    app.run(debug=True, host='127.3.7.9', port='8080')
